cf58c3197ef3796d2bf522e8580e25b9837aed5d
Tiger Cox <65963747+tigercoxgithub@users.noreply.github.com>
2022-12-05 13:10:26 +0000

    Update install-deps.sh

--- install-deps.sh
+++ install-deps.sh
@@ -1,16 +1,16 @@
 #!/bin/sh
 # Install dependecies
-sudo apt-get install -y pkg-config \
-sudo apt-get install -y libx264-dev \
-sudo apt-get install -y libgstreamer1.0-dev \
-sudo apt-get install -y libgstreamer-plugins-base1.0-dev \
-sudo apt-get install -y libgstreamer-plugins-bad1.0-dev \
-sudo apt-get install -y gstreamer1.0-plugins-base \
-sudo apt-get install -y gstreamer1.0-plugins-good \
-sudo apt-get install -y gstreamer1.0-plugins-bad \
-sudo apt-get install -y gstreamer1.0-plugins-ugly \
-sudo apt-get install -y gstreamer1.0-tools \
-sudo apt-get install -y gstreamer1.0-nice \
-sudo apt-get install -y libsoup2.4-dev \
-sudo apt-get install -y libglib2.0-dev \
-sudo apt-get install -y libjson-glib-dev \
+sudo apt install -y pkg-config &&\
+sudo apt install -y libx264-dev &&\
+sudo apt install -y libgstreamer1.0-dev &&\
+sudo apt install -y libgstreamer-plugins-base1.0-dev &&\
+sudo apt install -y libgstreamer-plugins-bad1.0-dev &&\
+sudo apt install -y gstreamer1.0-plugins-base &&\
+sudo apt install -y gstreamer1.0-plugins-good &&\
+sudo apt install -y gstreamer1.0-plugins-bad &&\
+sudo apt install -y gstreamer1.0-plugins-ugly &&\
+sudo apt install -y gstreamer1.0-tools &&\
+sudo apt install -y gstreamer1.0-nice &&\
+sudo apt install -y libsoup2.4-dev &&\
+sudo apt install -y libglib2.0-dev &&\
+sudo apt install -y libjson-glib-dev \
