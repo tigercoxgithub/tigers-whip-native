f5d5d44fe6a9a30f783e7ff5536ff1df344e400b
Tiger Cox <65963747+tigercoxgithub@users.noreply.github.com>
2022-12-05 12:52:12 +0000

    Create install-deps.sh
    just adds the gstreamer dev files etc

---
+++ install-deps.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+# Install dependecies
+sudo apt-get install -y pkg-config \
+sudo apt-get install -y libx264-dev \
+sudo apt-get install -y libgstreamer1.0-dev \
+sudo apt-get install -y libgstreamer-plugins-base1.0-dev \
+sudo apt-get install -y libgstreamer-plugins-bad1.0-dev \
+sudo apt-get install -y gstreamer1.0-plugins-base \
+sudo apt-get install -y gstreamer1.0-plugins-good \
+sudo apt-get install -y gstreamer1.0-plugins-bad \
+sudo apt-get install -y gstreamer1.0-plugins-ugly \
+sudo apt-get install -y gstreamer1.0-tools \
+sudo apt-get install -y gstreamer1.0-nice \
+sudo apt-get install -y libsoup2.4-dev \
+sudo apt-get install -y libglib2.0-dev \
+sudo apt-get install -y libjson-glib-dev \
